d49c20242ceaf5b63b77a621adbdd218dd166d6e
Zachary McDowell <cholmcdowell@gmail.com>
2025-07-20 22:34:14 -0500

    Update Request.h

--- include/Request.h
+++ include/Request.h
@@ -1,16 +1,36 @@
 #pragma once
 #include <string>
 
-struct Request{
-	std::string ip_in;
-	std::string ip_out;
-	int proc_time; // Processing time in cycles
+/**
+ * @struct Request
+ * @brief Represents a web request with input/output IPs and processing time.
+ */
+struct Request {
+    /// Input IP address.
+    std::string ip_in;
 
-	// Default Constructor
-	Request();
-	// Parameterized Constructor 
-	Request(const std::string& ip_in, const std::string& ip_out, int proc_time);
+    /// Output IP address.
+    std::string ip_out;
+
+    /// Processing time required (in cycles).
+    int proc_time;
+
+    /**
+     * @brief Default constructor.
+     */
+    Request();
+
+    /**
+     * @brief Parameterized constructor.
+     * @param ip_in Source IP address.
+     * @param ip_out Destination IP address.
+     * @param proc_time Processing time in cycles.
+     */
+    Request(const std::string& ip_in, const std::string& ip_out, int proc_time);
 };
 
-// Random IP generator
+/**
+ * @brief Generates a random IP address in string format.
+ * @return A randomly generated IP address.
+ */
 std::string rand_ip();
