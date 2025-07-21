40caaf49ed2151cabbda62be30dcf2691663305f
cholmcdowell <cholmcdowell@gmail.com>
2025-07-21 01:42:35 +0000

    WebServer

---
+++ include/WebServer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Request.h"
+#include <queue>
+
+class WebServer {
+
+public:
+    int server_id;
+    int available_at;  // cycle count when server is free again
+    std::queue<Request> queue;
+    WebServer(int id = 0);
+    void enqueue(const Request& req);
+    void try_process(int current_cycle);
+    bool is_available(int current_cycle) const;
+    int get_queue_length() const;
+};
+
---
+++ src/WebServer.cpp
@@ -0,0 +1,26 @@
+#include "WebServer.h"
+
+
+WebServer::WebServer(int server_id) : server_id(server_id), available_at(0) {}
+
+
+void WebServer::enqueue(const Request& req) {
+    queue.push(req);
+}
+
+void WebServer::try_process(int current_cycle) {
+    if (!queue.empty() && is_available(current_cycle)) {
+        Request req = queue.front();
+        queue.pop();
+        available_at = current_cycle + req.proc_time;
+    }
+}
+
+
+bool WebServer::is_available(int current_cycle) const {
+    return current_cycle >= available_at;
+}
+
+int WebServer::get_queue_length() const {
+    return queue.size();
+}
